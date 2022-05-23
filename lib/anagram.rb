6664d40fa3f2dcdfdd1a87ea357c800382c2f933
Hyun Sung Cho <hcho774@gmail.com>
2022-05-23 13:43:46 -0400

    commit tree 39ca5f50e2c34490be011f8bb87c48a3adaa0501

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_accessor :input
+  def initialize(input)
+    @word = input
+  end
+
+  def match(arr)
+    arr.select do |letter|
+    letter.split("").sort == @word.split("").sort
+    end
+  end
+
+
+end
+
+
+listen = Anagram.new("listen")
+puts listen.match(%w[enlists google inlets banana])
